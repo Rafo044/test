4268844f3c4120c4ebe3e590a9dace51afc16133
Rafo044 <rafo044@protonmail.com>
2025-11-01 20:39:00 +0400

    Change authentificaten method and add xcom_pull

--- airflow/dags/authentification.py
+++ airflow/dags/authentification.py
@@ -2,52 +2,27 @@
 
 import os
 import requests
+
 from dotenv import load_dotenv
 
-
-from airflow.operators.python import PythonOperator
-from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.sdk import DAG
-import pendulum
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 FASTAPI_URL = os.getenv("FASTAPI_URL")
 
-authentification = DAG(
-    dag_id="authentification",
-    start_date=pendulum.datetime(2023, 1, 1),
-    schedule="@once",
-)
 
-
-def authenticate():
+def authenticate(method, endpoint, **kwargs):
     """FastAPI servisinin authentikifasiyasını yoxlama funksiyası
+
+    Args:
+        method (str): HTTP methodu (GET, POST, PUT, DELETE)
+        endpoint (str): API endpointi məsələn "all" ,"/" yazmaq olmaz.
+        **kwargs: kwargs
     Returns:
-        Success: Autentifikasiya uygurludur.
-        Failure: Autentifikasiya yalnışdır.
+        response : Request olunacaq funksiya.
     """
     headers = {"Authorization": f"Bearer {TOKEN}"}
-    response = requests.get(FASTAPI_URL, headers=headers)
-
-    if response.status_code == 200:
-        print("Success:", response.json())
-    else:
-        print("Failed:", response.status_code, response.json())
-
-
-task = PythonOperator(
-    task_id="authenticate",
-    python_callable=authenticate,
-    dag=authentification,
-)
-
-trigger_run = TriggerDagRunOperator(
-    task_id="trigger_run",
-    dag=authentification,
-    trigger_dag_id="sales_payment",
-    trigger_run_id="sales_payment_run",
-)
-
-task >> trigger_run
+    url = f"{FASTAPI_URL}/{endpoint}"
+    response = requests.request(method, url, headers=headers, **kwargs)
+    return response
--- airflow/dags/sales_payment.py
+++ airflow/dags/sales_payment.py
@@ -1,8 +1,17 @@
+"""Imports"""
+
+from datetime import datetime
+
 from airflow.sdk import DAG, task
 from airflow.providers.standard.operators.bash import BashOperator
 from airflow.decorators import task_group
-from datetime import datetime
+import pandas as pd
 
+from authentification import authenticate
+
+# =====================================================================
+#                     DAG
+# ====================================================================
 
 sales_payment = DAG(
     dag_id="sales_payment",
@@ -11,7 +20,74 @@ sales_payment = DAG(
     catchup=False,
 )
 
+# =====================================================================
+#                    HELPER FUNCTIONS
+# ====================================================================
 
-@task_group(group_id="wait_for_auth")
-def wait_for_auth():
-    task1 = EmptyOperator(task_id="wait_for_auth")
+
+def load_data(**context):
+    """
+    Restapi dan datanı çəkən funksiya.
+    """
+    response = authenticate(GET, "all")
+    if response.status_code == 200:
+        data = response.json()
+        context["ti"].xcom_push(key="data", value=data)
+    else:
+        raise Exception(f"Failed to load data: {response.status_code}")
+
+
+def read_data(**context):
+    """
+    Datani temizleyen funksiya.
+    """
+    data = context["ti"].xcom_pull(key="data")
+    read_data = pd.DataFrame(data)
+    context["ti"].xcom_push(key="data", value=read_data)
+
+
+def clean_data(**context):
+    """
+    Datani temizleyen funksiya.
+    """
+    data = context["ti"].xcom_pull(key="data", value=read_data)
+    clean_data = data.dropna()
+    context["ti"].xcom_push(key="data", value=clean_data)
+
+
+# ====================================================================
+#                     TASKS
+# ====================================================================
+
+
+@task_group(group_id="load_data")
+def load_data_task():
+    task2 = EmptyOperator(task_id="waiting_load_data")
+    task3 = PythonOperator(
+        task_id="load_data",
+        python_callable=load_data,
+    )
+    task2 >> task3
+
+
+@task_group(group_id="read_data")
+def read_data_task():
+    task4 = EmptyOperator(task_id="waiting_read_data")
+    task5 = PythonOperator(
+        task_id="read_data",
+        python_callable=read_data,
+    )
+    task4 >> task5
+
+
+@task_group(group_id="clean_data")
+def clean_data_task():
+    task6 = EmptyOperator(task_id="waiting_clean_data")
+    task7 = PythonOperator(
+        task_id="clean_data",
+        python_callable=clean_data,
+    )
+    task6 >> task7
+
+
+load_data_task() >> read_data_task() >> clean_data_task()
